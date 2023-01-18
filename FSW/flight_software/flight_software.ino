10c3fe00642a9d1fc07258a41e9fd2e948ee763e
stzfao <aoyon.stzf@gmail.com>
2023-01-19 00:38:15 +0530

    added state related function definitions

--- FSW/flight_software/flight_software.ino
+++ FSW/flight_software/flight_software.ino
@@ -1,9 +1,14 @@
 #include <EEPROM.h>
 #include <Wire.h>
-#include<SoftwareSerial.h>
+#include <SoftwareSerial.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BME680.h"
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+
+#define EEPROM_SIZE 1
 
 #define LAUNCH_PAD 1
 #define ASCENT 2
@@ -13,11 +18,6 @@
 
 //TELEMETRY DEFINITIONS
 #define TEAM_ID (2022ASI-005)
-
-#define BME_SCK 18
-#define BME_MISO 19
-#define BME_MOSI 23
-#define BME_CS 15
 
 //check these pins later
 #define XbeeTxPin 12 // xbee's tx
@@ -57,14 +57,54 @@ struct gyro_data
   
 }
 
-Adafruit_BME680 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK);
+Adafruit_BME680 bme;
 SoftwareSerial Xbee = SoftwareSerial(XbeeRxPin, XbeeTxPin);
+uint8_t P1 = 0, P2 = 0; 
 
-void setup() {
+int STATE = 1;
 
+int ascending()
+{
+  // return -1 if altitude decreasing, 1 if altitude increasing, 0 if constant
+}
+
+void setup() 
+{
+  // check last saved state and update as needed
+  EEPROM.begin(EEPROM_SIZE);
+  STATE = EEPROM.read(0);
+  // may need this saved state in the future
+
+  setupSD();
+  //check last saved state
+  calibrateSensors();
+  
+  //XBee comms start 
+  Xbee.begin(9600);
+}
+
+void setupSD()
+{
+  Serial.begin(115200);
+  if(!SD.begin()){
+    Serial.println("Card Mount Failed");
+    return;
+  }
+
+  uint8_t cardType = SD.cardType();
+
+  if(cardType == CARD_NONE){
+    Serial.println("No SD card attached");
+    return;
+  }
+}
+
+void calibrateSensors()
+{
   //BME680
   {
-    while(!bme.begin()) {
+    while(!bme.begin()) 
+    {
       Serial.println(F("Could not find a valid BME680 sensor, check wiring!"));
     }
 
@@ -75,11 +115,6 @@ void setup() {
     bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
     bme.setGasHeater(320, 150); // 320*C for 150 ms
   }
-
-  //XBee
-  Xbee.begin(9600);
-
-
 }
 
 void loop(){
@@ -102,13 +137,48 @@ void loop(){
   }
 }
 
+// append to CSV file
+// appendFile(SD, "/final.csv", "2022ASI-005,0022,0022,2000,101.3,27.3,3.5,030742.00,02232.73830,11404.58520,32908.09,12,10,2145,1,3.4\n");
 
-void send_TelemetryData()
+void SDNewLine(fs::FS &fs, const char * path, const char * message)
+{
+  Serial.printf("Appending to file: %s\n", path);
+
+  File file = fs.open(path, FILE_APPEND);
+  if(!file){
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  if(file.print(message)){
+    Serial.println("Message appended");
+  } else {
+    Serial.println("Append failed");
+  }
+  file.close();
+}
+
+void saveToEEPROM()
 {
   
 }
 
-void get_MPU9255_Data()
+void send_TelemetryData()
+{
+
+  // create telemetry string
+  // telemetry_string = 
+  // send string by downlink
+  // save string to SD card
+  SDNewLine(SD, telemetry_string);
+  
+}
+
+void get_TelemetryCommands()
+{
+  
+}
+
+void get_MPU9250_Data()
 {
 
 }
@@ -134,27 +204,43 @@ void get_BME680_Data()
   payload_data.VOC_sensor = bme.gas_resistance;
 }
 
-void runState_LAUNCH_PAD()
+void audioBeacon()
 {
   
 }
 
-void runState_ASCENT()
+void runState_LAUNCH_PAD()
 {
-  
+  EEPROM.write(0, STATE);
+  if(ascending() > 0) STATE = ASCENT;
+}
+
+void runState_ASCENT()
+{ 
+  EEPROM.write(0, STATE);  
+  if(ascending() < 0){
+    STATE =  (telemetry_data.altitude > 500) ? DESCENT_1 : DESCENT_2;
+  } 
+  send_TelemetryData();
 }
 
 void runState_DESCENT_1()
 {
-  
+  EEPROM.write(0, STATE);  
+  if(ascending() < 0 && telemetry_data.altitude <= 500) STATE = DESCENT_2;  
+  send_TelemetryData();
 }
 
 void runState_DESCENT_2()
 {
-  
+  EEPROM.write(0, STATE);  
+  if(ascending() == 0) STATE = LANDED;  
+  send_TelemetryData();
 }
 
 void runState_LANDED()
 {
-  
+  EEPROM.write(0, STATE);  
+  send_TelemetryData();
+  audioBeacon();
 }
